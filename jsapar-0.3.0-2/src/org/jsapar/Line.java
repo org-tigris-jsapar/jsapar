4d2b473acd1bab117b756d558f8a209032609329
stenix71 <p.jonas.stenberg@gmail.com>
2008-11-25 08:46:24 +0000

    Version 0.3.0-2 released.

---
+++ jsapar-0.3.0-2/src/org/jsapar/Line.java
@@ -0,0 +1,188 @@
+package org.jsapar;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+/**
+ * A line is one row of the input buffer. Each line contains a list of cells. Cells can be retreived
+ * either by index O(1) or by name O(n). Note that the class is not synchronized internally. If
+ * multiple threads access the same instance, external synchronization is required.
+ * 
+ * @author Jonas Stenberg
+ * 
+ */
+public class Line implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6026541900371948402L;
+
+    private java.util.ArrayList<Cell> cellsByIndex = null;
+
+    private java.util.HashMap<String, Cell> cellsByName = null;
+
+    /**
+     * Line type.
+     */
+    private String lineType;
+
+    /**
+     * Creates an empty line without any cells.
+     */
+    public Line() {
+	this.cellsByIndex = new java.util.ArrayList<Cell>();
+	this.cellsByName = new java.util.HashMap<String, Cell>();
+    }
+
+    /**
+     * Creates an empty line without any cells but with an initial capacity.
+     * 
+     * @param nInitialCapacity
+     *            The initial capacity. Used only to reserve space. If capacity is exceeded, the
+     *            capacity grows automatically.
+     */
+    public Line(int nInitialCapacity) {
+	this.cellsByIndex = new java.util.ArrayList<Cell>(nInitialCapacity);
+	this.cellsByName = new java.util.HashMap<String, Cell>(nInitialCapacity);
+
+    }
+
+    /**
+     * Creates an empty line of a specified type, without any cells but with an initial capacity.
+     * 
+     * @param sLineType
+     *            The type of the line.
+     */
+    public Line(String sLineType) {
+	this();
+	lineType = sLineType;
+    }
+
+    /**
+     * Creates an empty line of a specified type, without any cells but with an initial capacity.
+     * 
+     * @param sLineType
+     *            The type of the line.
+     * @param nInitialCapacity
+     *            The initial capacity. Used only to reserve space. If capacity is exceeded, the
+     *            capacity grows automatically.
+     */
+    public Line(String sLineType, int nInitialCapacity) {
+	this(nInitialCapacity);
+	lineType = sLineType;
+    }
+
+    /**
+     * For better performance while iterating multiple lines, it is better to call the
+     * {@link #getCellIterator()} method.
+     * 
+     * @return A clone of the internal collection that contains all the cells of this documents.
+     *         Altering the returned collection will not alter the original collection of the this
+     *         Line.
+     * @see #getCellIterator()
+     */
+    @SuppressWarnings("unchecked")
+    public java.util.List<Cell> getCells() {
+	return (java.util.List<Cell>) cellsByIndex.clone();
+    }
+
+    /**
+     * Returns an iterator that will iterate all the cells of this line.
+     * 
+     * @return An iterator that will iterate all the cells of this line.
+     */
+    public Iterator<Cell> getCellIterator() {
+	return cellsByIndex.iterator();
+    }
+
+    /**
+     * Adds a cell to the end of the line.
+     * 
+     * @param cell
+     *            The cell to add
+     */
+    public void addCell(Cell cell) {
+	this.cellsByIndex.add(cell);
+	if (cell.getName() != null)
+	    this.cellsByName.put(cell.getName(), cell);
+    }
+
+    /**
+     * Adds a cell at specified index of a line. First cell has index 0. Existing cells to the rigth
+     * of the new cell will have incremented indexes.
+     * 
+     * @param cell
+     *            The cell to add
+     * @param index
+     *            The index the cell will have in the line.
+     */
+    public void addCell(Cell cell, int index) {
+	this.cellsByIndex.add(index, cell);
+	if (cell.getName() != null)
+	    this.cellsByName.put(cell.getName(), cell);
+    }
+
+    /**
+     * Gets a cell at specified index. First cell has index 0.
+     * 
+     * @param index
+     * @return The cell
+     */
+    public Cell getCell(int index) {
+	return this.cellsByIndex.get(index);
+    }
+
+    /**
+     * Gets a cell with specified name. Name is specified by the schema.
+     * 
+     * @param name
+     * @return The cell
+     */
+    public Cell getCell(String name) {
+	return this.cellsByName.get(name);
+    }
+
+    /**
+     * Gets the number of cells that this line contains.
+     * 
+     * @return the number of cells that this line contains.
+     */
+    public int getNumberOfCells() {
+	return this.cellsByIndex.size();
+    }
+
+    /**
+     * Returns the type of this line. The line type attribute is primarily used when parsing lines
+     * of different types, distinguished by a control cell.
+     * 
+     * @return the lineType
+     */
+    public String getLineType() {
+	return lineType;
+    }
+
+    /**
+     * Sets the type of this line. The line type attribute is primarily used when parsing lines of
+     * different types, distinguished by a control cell.
+     * 
+     * @param lineType
+     *            the lineType to set
+     */
+    public void setLineType(String lineType) {
+	this.lineType = lineType;
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder sb = new StringBuilder();
+	if (this.lineType != null && this.lineType.length() > 0) {
+	    sb.append("Line type=");
+	    sb.append(this.lineType);
+	    sb.append(", ");
+	}
+	sb.append("Cells: ");
+	sb.append(this.cellsByIndex);
+	return sb.toString();
+    }
+}
---
+++ jsapar-0.3.0-2/test/org/jsapar/schema/Xml2SchemaBuilderTest.java
@@ -0,0 +1,97 @@
+/** 
+ * Copyrigth: Jonas Stenberg
+ */
+package org.jsapar.schema;
+
+import static org.junit.Assert.assertEquals;
+
+import org.jsapar.Cell.CellType;
+import org.junit.Test;
+
+/**
+ * @author Jonas
+ * 
+ */
+public class Xml2SchemaBuilderTest {
+
+    /**
+     * Test method for {@link org.jsapar.schema.Xml2SchemaBuilder#build(java.io.Reader)} .
+     * 
+     * @throws SchemaException
+     */
+    @Test
+    public final void testBuild_FixedLength() throws SchemaException {
+
+	String sXmlSchema = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+		+ "<schema  xmlns=\"http://jsapar.tigris.org/JSaParSchema/1.0\" >"
+		+ "<fixedwidthschema lineseparator=\"&#13;&#10;\"><line occurs=\"*\">"
+		+ "<cell name=\"First name\" length=\"5\"/>" + "<cell name=\"Last name\" length=\"8\"/>"
+		+ "<cell name=\"Shoe size\" length=\"8\"><format type=\"integer\"/></cell>"
+		+ "</line></fixedwidthschema></schema>";
+
+	Xml2SchemaBuilder builder = new Xml2SchemaBuilder();
+	java.io.Reader reader = new java.io.StringReader(sXmlSchema);
+	Schema schema = builder.build(reader);
+	FixedWidthSchema fwSchema = (FixedWidthSchema) schema;
+
+	assertEquals("\r\n", fwSchema.getLineSeparator());
+
+	assertEquals("First name", fwSchema.getFixedWidthSchemaLines().get(0).getSchemaCells().get(0).getName());
+	assertEquals("Last name", fwSchema.getFixedWidthSchemaLines().get(0).getSchemaCells().get(1).getName());
+
+	assertEquals(5, fwSchema.getFixedWidthSchemaLines().get(0).getSchemaCells().get(0).getLength());
+	assertEquals(8, fwSchema.getFixedWidthSchemaLines().get(0).getSchemaCells().get(1).getLength());
+
+	assertEquals(CellType.INTEGER, fwSchema.getFixedWidthSchemaLines().get(0).getSchemaCells().get(2)
+		.getCellFormat().getCellType());
+
+    }
+
+    @Test
+    public final void testBuild_Csv() throws SchemaException {
+
+	String sXmlSchema = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+		+ "<schema  xmlns=\"http://jsapar.tigris.org/JSaParSchema/1.0\" >" + "<csvschema><line occurs=\"4\">"
+		+ "<cell name=\"First name\"/>" + "<cell name=\"Last name\"/>" + "</line></csvschema></schema>";
+
+	Xml2SchemaBuilder builder = new Xml2SchemaBuilder();
+	java.io.Reader reader = new java.io.StringReader(sXmlSchema);
+	Schema schema = builder.build(reader);
+	CsvSchema csvSchema = (CsvSchema) schema;
+
+	assertEquals("First name", csvSchema.getCsvSchemaLines().get(0).getSchemaCells().get(0).getName());
+	assertEquals("Last name", csvSchema.getCsvSchemaLines().get(0).getSchemaCells().get(1).getName());
+
+    }
+
+    @Test
+    public final void testBuild_Csv_firstlineasschema() throws SchemaException {
+
+	String sXmlSchema = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+		+ "<schema  xmlns=\"http://jsapar.tigris.org/JSaParSchema/1.0\" >"
+		+ "<csvschema><line occurs=\"4\" firstlineasschema=\"true\" >" + "</line></csvschema></schema>";
+
+	Xml2SchemaBuilder builder = new Xml2SchemaBuilder();
+	java.io.Reader reader = new java.io.StringReader(sXmlSchema);
+	Schema schema = builder.build(reader);
+	CsvSchema csvSchema = (CsvSchema) schema;
+
+	assertEquals(true, csvSchema.getCsvSchemaLines().get(0).isFirstLineAsSchema());
+
+    }
+
+    @Test(expected = SchemaException.class)
+    public final void testBuild_Csv_firstlineasschema_error() throws SchemaException {
+
+	// "yes" is not a valid boolean value.
+	String sXmlSchema = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+		+ "<schema  xmlns=\"http://jsapar.tigris.org/JSaParSchema/1.0\" >\n"
+		+ "<csvschema><line occurs=\"4\" firstlineasschema=\"yes\" >\n" + "</line></csvschema></schema>";
+
+	Xml2SchemaBuilder builder = new Xml2SchemaBuilder();
+	java.io.Reader reader = new java.io.StringReader(sXmlSchema);
+	@SuppressWarnings("unused")
+	Schema schema = builder.build(reader);
+    }
+
+}
