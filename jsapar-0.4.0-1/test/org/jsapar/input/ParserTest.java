1f7135ce665439f42cc1dca99c30094c10ac9711
stenix71 <p.jonas.stenberg@gmail.com>
2009-01-19 12:24:29 +0000

    Release 0.4

---
+++ jsapar-0.4.0-1/test/org/jsapar/input/ParserTest.java
@@ -0,0 +1,89 @@
+package org.jsapar.input;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.jsapar.Document;
+import org.jsapar.JSaParException;
+import org.jsapar.schema.FixedWidthSchemaCell;
+import org.jsapar.schema.FixedWidthSchemaLine;
+import org.junit.Test;
+
+
+public class ParserTest {
+
+    @Test
+    public void testBuild_fixed_oneLine() throws JSaParException {
+        String toParse = "JonasStenberg";
+        org.jsapar.schema.FixedWidthSchema schema = new org.jsapar.schema.FixedWidthSchema();
+        FixedWidthSchemaLine schemaLine = new FixedWidthSchemaLine(1);
+        schemaLine.addSchemaCell(new FixedWidthSchemaCell("First name", 5));
+        schemaLine.addSchemaCell(new FixedWidthSchemaCell("Last name", 8));
+        schema.addSchemaLine(schemaLine);
+
+        Parser builder = new Parser(schema);
+        Reader reader = new StringReader(toParse);
+        Document doc = builder.build(reader);
+
+        assertEquals(1, doc.getNumberOfLines());
+        assertEquals("Jonas", doc.getLine(0).getCell(0).getStringValue());
+        assertEquals("Stenberg", doc.getLine(0).getCell("Last name").getStringValue());
+    }
+
+    @Test
+    public void testBuild_fixed_twoLines() throws JSaParException {
+        String toParse = "JonasStenberg" + System.getProperty("line.separator") + "FridaStenberg";
+        org.jsapar.schema.FixedWidthSchema schema = new org.jsapar.schema.FixedWidthSchema();
+        FixedWidthSchemaLine schemaLine = new FixedWidthSchemaLine(2);
+        schemaLine.addSchemaCell(new FixedWidthSchemaCell("First name", 5));
+        schemaLine.addSchemaCell(new FixedWidthSchemaCell("Last name", 8));
+        schema.addSchemaLine(schemaLine);
+
+        Parser builder = new Parser(schema);
+        Reader reader = new StringReader(toParse);
+        Document doc = builder.build(reader);
+
+        assertEquals(2, doc.getNumberOfLines());
+        assertEquals("Jonas", doc.getLine(0).getCell(0).getStringValue());
+        assertEquals("Stenberg", doc.getLine(0).getCell("Last name").getStringValue());
+
+        assertEquals("Frida", doc.getLine(1).getCell(0).getStringValue());
+        assertEquals("Stenberg", doc.getLine(1).getCell("Last name").getStringValue());
+    }
+
+    @Test
+    public void testBuild_fixed_twoLines_toLong() throws JSaParException {
+        String toParse = "Jonas " + System.getProperty("line.separator") + "Frida";
+        org.jsapar.schema.FixedWidthSchema schema = new org.jsapar.schema.FixedWidthSchema();
+        FixedWidthSchemaLine schemaLine = new FixedWidthSchemaLine(2);
+        schemaLine.addSchemaCell(new FixedWidthSchemaCell("First name", 5));
+        schema.addSchemaLine(schemaLine);
+
+        Parser builder = new Parser(schema);
+        Reader reader = new StringReader(toParse);
+        Document doc = builder.build(reader);
+        assertEquals(2, doc.getNumberOfLines());
+        assertEquals("Jonas", doc.getLine(0).getCell(0).getStringValue());
+        assertEquals("Frida", doc.getLine(1).getCell(0).getStringValue());
+    }
+
+    @Test
+    public void testBuild_fixed_twoLines_infiniteOccurs() throws JSaParException {
+        String toParse = "Jonas" + System.getProperty("line.separator") + "Frida";
+        org.jsapar.schema.FixedWidthSchema schema = new org.jsapar.schema.FixedWidthSchema();
+        FixedWidthSchemaLine schemaLine = new FixedWidthSchemaLine();
+        schemaLine.setOccursInfinitely();
+        schemaLine.addSchemaCell(new FixedWidthSchemaCell("First name", 5));
+        schema.addSchemaLine(schemaLine);
+
+        Parser builder = new Parser(schema);
+        Reader reader = new StringReader(toParse);
+        Document doc = builder.build(reader);
+
+        assertEquals("Jonas", doc.getLine(0).getCell(0).getStringValue());
+        assertEquals("Frida", doc.getLine(1).getCell(0).getStringValue());
+    }
+    
+}
