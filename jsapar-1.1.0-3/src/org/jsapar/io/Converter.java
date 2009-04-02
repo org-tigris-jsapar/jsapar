ed6e43d24a32644340e393d4d492cd1ec1be4f03
stenix71 <p.jonas.stenberg@gmail.com>
2009-04-02 06:46:14 +0000

    Build jsapar-1.1.0-3

---
+++ jsapar-1.1.0-3/src/org/jsapar/io/Converter.java
@@ -0,0 +1,258 @@
+package org.jsapar.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import org.jsapar.JSaParException;
+import org.jsapar.Line;
+import org.jsapar.input.CellParseError;
+import org.jsapar.input.LineErrorEvent;
+import org.jsapar.input.LineParsedEvent;
+import org.jsapar.input.ParseSchema;
+import org.jsapar.input.ParsingEventListener;
+import org.jsapar.schema.Schema;
+import org.jsapar.schema.SchemaException;
+
+/**
+ * Reads buffer using an input schema and writes to another buffer using an output schema. By adding
+ * a LineManipulator you are able to make modifications of each line before it is written to the
+ * output. The method manipulate() of all added LineManipulators are called for each line that are
+ * parsed successfully.
+ * 
+ * @author stejon0
+ * 
+ */
+public class Converter {
+
+    private List<LineManipulator> manipulators = new java.util.LinkedList<LineManipulator>();
+    private ParseSchema inputSchema;
+    private Schema outputSchema;
+
+    /**
+     * Creates a Converter object with the specified schemas.
+     * 
+     * @param inputSchema
+     * @param outputSchema
+     */
+    public Converter(ParseSchema inputSchema, Schema outputSchema) {
+        this.inputSchema = inputSchema;
+        this.outputSchema = outputSchema;
+    }
+
+    /**
+     * Adds LineManipulator to this converter. All present line manipulators are executed for each
+     * line.
+     * 
+     * @param manipulator
+     */
+    public void addLineManipulator(LineManipulator manipulator) {
+        manipulators.add(manipulator);
+    }
+
+    /**
+     * @param reader
+     * @param writer
+     * @return A list of CellParseErrors or an empty list if there were no errors.
+     * @throws IOException
+     * @throws JSaParException
+     */
+    public java.util.List<CellParseError> convert(java.io.Reader reader, java.io.Writer writer) throws IOException,
+            JSaParException {
+
+        DocumentWriter outputter = new DocumentWriter(outputSchema, writer);
+        // TODO Create a DocumentWriter that supports line type by control cell.
+
+        outputSchema.outputBefore(writer);
+        inputSchema.parse(reader, outputter);
+        outputSchema.outputAfter(writer);
+        return outputter.getParseErrors();
+
+    }
+
+    /**
+     * Internal class for handling output of one line at a time while receiving parsing events.
+     * 
+     * @author stejon0
+     * 
+     */
+    private class DocumentWriter implements ParsingEventListener {
+        private List<CellParseError> parseErrors = new LinkedList<CellParseError>();
+        private Schema outputSchema;
+        private java.io.Writer writer;
+
+        public DocumentWriter(Schema outputSchema, Writer writer) throws JSaParException {
+            this.outputSchema = outputSchema;
+            this.writer = writer;
+        }
+
+        @Override
+        public void lineErrorEvent(LineErrorEvent event) {
+            parseErrors.add(event.getCellParseError());
+        }
+
+        @Override
+        public void lineParsedEvent(LineParsedEvent event) throws JSaParException {
+            try {
+                Line line = event.getLine();
+                for (LineManipulator manipulator : manipulators) {
+                    manipulator.manipulate(line);
+                }
+                outputSchema.outputLine(line, event.getLineNumber(), this.writer);
+            } catch (IOException e) {
+                throw new JSaParException("Failed to write to writer", e);
+            }
+        }
+
+        /**
+         * @return the parseErrors
+         */
+        public List<CellParseError> getParseErrors() {
+            return parseErrors;
+        }
+
+    }
+
+    /**
+     * @return the inputSchema
+     */
+    public ParseSchema getInputSchema() {
+        return inputSchema;
+    }
+
+    /**
+     * @param inputSchema
+     *            the inputSchema to set
+     */
+    public void setInputSchema(ParseSchema inputSchema) {
+        this.inputSchema = inputSchema;
+    }
+
+    /**
+     * @return the outputSchema
+     */
+    public Schema getOutputSchema() {
+        return outputSchema;
+    }
+
+    /**
+     * @param outputSchema
+     *            the outputSchema to set
+     */
+    public void setOutputSchema(Schema outputSchema) {
+        this.outputSchema = outputSchema;
+    }
+
+    /**
+     * Reads command line arguments into property structure.
+     * 
+     * @param properties
+     *            The properties to be filled with arguments.
+     * @param args
+     *            The arguments.
+     * @return
+     */
+    private static void readArgs(Properties properties, String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (arg.length() > 1 && arg.charAt(0) == '-') {
+                if (args.length > i + 1) {
+                    properties.setProperty(arg.substring(1, arg.length()), args[i + 1]);
+                    i++; // Skip next.
+                }
+            }
+        }
+    }
+
+    private static void checkMandatory(Properties properties, String key) throws JSaParException {
+        if (null == properties.getProperty(key))
+            throw new JSaParException("Mandatory argument -" + key + " is missing.");
+    }
+
+    /**
+     * @param args
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws JSaParException
+     */
+    private static Properties readConfig(String[] args) throws FileNotFoundException, IOException, JSaParException {
+        Properties properties = new Properties();
+        if (args.length == 1) {
+            properties.load(new FileReader(args[1]));
+        } else if (args.length > 1) {
+            readArgs(properties, args);
+        }
+        // Check mandatory arguments
+        checkMandatory(properties, "in.schema");
+        checkMandatory(properties, "out.schema");
+        checkMandatory(properties, "in.file");
+        return properties;
+    }
+
+    /**
+     * @param fileName
+     * @return
+     * @throws SchemaException
+     * @throws IOException
+     */
+    private static Schema loadSchemaFromXmlFile(String fileName) throws SchemaException, IOException {
+        Reader schemaReader = new FileReader(fileName);
+        org.jsapar.schema.Xml2SchemaBuilder builder = new org.jsapar.schema.Xml2SchemaBuilder();
+        Schema schema = builder.build(schemaReader);
+        schemaReader.close();
+        return schema;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        final String applicationName = "jsapar.jar";
+        Properties properties;
+        try {
+            properties = readConfig(args);
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            System.out.println("");
+            System.out.println("Usage:");
+            System.out.println(" 1. " + applicationName + " <property file name> ");
+            System.out.println(" 2. " + applicationName
+                    + " -in.schema <input schem name> -out.schema <output schema name>");
+            System.out.println("               -in.file <input file name> [-out.file <output file name>]");
+            System.out.println("");
+            System.out.println("Alternative 1. above reads the arguments from a property file.");
+            return;
+        }
+
+        try {
+            Schema inputSchema = loadSchemaFromXmlFile(properties.getProperty("in.schema"));
+            Schema outputSchema = loadSchemaFromXmlFile(properties.getProperty("out.schema"));
+
+            String inFileName = properties.getProperty("in.file");
+            Reader inputFileReader = new java.io.FileReader(inFileName);
+            java.io.Writer writer = new java.io.FileWriter(properties.getProperty("out.file", inFileName + ".out"));
+
+            Converter converter = new Converter(inputSchema, outputSchema);
+            java.util.List<CellParseError> parseErrors = converter.convert(inputFileReader, writer);
+
+            if (parseErrors.size() > 0)
+                System.out.println("===> Found errors while converting file " + inFileName + ": "
+                        + System.getProperty("line.separator") + parseErrors);
+            else
+                System.out.println("Successfully converted file " + inFileName);
+
+            inputFileReader.close();
+            writer.close();
+        } catch (Throwable t) {
+            System.err.println("Failed to convert file.");
+            t.printStackTrace(System.err);
+        }
+    }
+
+}
---
+++ jsapar-1.1.0-3/src/org/jsapar/schema/SchemaLine.java
@@ -0,0 +1,266 @@
+package org.jsapar.schema;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import org.jsapar.Cell;
+import org.jsapar.JSaParException;
+import org.jsapar.Line;
+import org.jsapar.input.LineParsedEvent;
+import org.jsapar.input.ParsingEventListener;
+
+public abstract class SchemaLine implements Cloneable {
+    private static final int OCCURS_INFINITE = Integer.MAX_VALUE;
+
+    private int occurs = 1;
+
+    private String lineType;
+
+    private String lineTypeControlValue;
+
+    private boolean ignoreReadEmptyLines = true;
+
+    public SchemaLine() {
+        this.occurs = OCCURS_INFINITE;
+    }
+
+    /**
+     * Creates a SchemaLine which occurs supplied number of times.
+     * 
+     * @param nOccurs
+     *            The number of times that a line of this type occurs in the corresponding buffer.
+     */
+    public SchemaLine(int nOccurs) {
+        this.occurs = nOccurs;
+    }
+
+    /**
+     * Creates a SchemaLine with the supplied line type.
+     * 
+     * @param lineType
+     *            The name of the type of the line.
+     */
+    public SchemaLine(String lineType) {
+        this.setLineType(lineType);
+    }
+
+    /**
+     * Creates a SchemaLine with the supplied line type and control value.
+     * 
+     * @param lineType
+     *            The name of the type of the line.
+     * @param lineTypeControlValue
+     *            The tag that determines which type of line it is.
+     */
+    public SchemaLine(String lineType, String lineTypeControlValue) {
+        this(lineType);
+        this.setLineTypeControlValue(lineTypeControlValue);
+    }
+
+    /**
+     * @return The number of times this type of line occurs in the corresponding buffer.
+     */
+    public int getOccurs() {
+        return occurs;
+    }
+
+    /**
+     * @param occurs
+     *            The number of times this type of line occurs in the corresponding buffer.
+     */
+    public void setOccurs(int occurs) {
+        this.occurs = occurs;
+    }
+
+    /**
+     * Setts the occurs attribute so that this type of line occurs until the end of the buffer.
+     */
+    public void setOccursInfinitely() {
+        this.occurs = OCCURS_INFINITE;
+    }
+
+    /**
+     * @return true if this line occurs to the end of the buffer, false otherwise.
+     */
+    public boolean isOccursInfinitely() {
+        return this.occurs == OCCURS_INFINITE;
+    }
+
+    /**
+     * Finds the cell to use for output. Each cell is identified from the schema by the name of the
+     * cell. If the schema-cell has no name, the cell at the same position in the line is used under
+     * the condition that it also lacks name.
+     * 
+     * If the schema-cell has a name the cell with the same name is used. If no such cell is found
+     * and the cell att the same position lacks name, it is used instead.
+     * 
+     * 
+     * @param line
+     *            The line to find a cell within.
+     * @param schemaCell
+     *            The schema-cell to use.
+     * @param nSchemaCellIndex
+     *            The index at wich the schema-cell is found.
+     * @return The cell within the supplied line to use for output according to the supplied
+     *         schemaCell.
+     */
+    protected static Cell findCell(Line line, SchemaCell schemaCell, int nSchemaCellIndex) {
+        Cell cellByIndex = null;
+        if (nSchemaCellIndex < line.getNumberOfCells())
+            cellByIndex = line.getCell(nSchemaCellIndex); // Use optimistic
+        // matching.
+        if (null != cellByIndex && null == schemaCell.getName()) {
+            // cell = line.getCell(i);
+            if (null == cellByIndex.getName()) {
+                // If both cell and schema cell names are null then it is ok
+                // to use cell by index.
+                return cellByIndex;
+            } else {
+                return null;
+            }
+        } else {
+            if (null != cellByIndex && null != cellByIndex.getName()
+                    && schemaCell.getName().equals(cellByIndex.getName())) {
+                // We were lucky.
+                return cellByIndex;
+            } else {
+                // The optimistic match failed. We take the penalty.
+                Cell cellByName = line.getCell(schemaCell.getName());
+                if (null != cellByName) {
+                    return cellByName;
+                } else {
+                    if (null == cellByIndex) {
+                        return null;
+                    } else if (cellByIndex.getName() == null) {
+                        // If it was not found by name we fall back to the
+                        // cell with correct index.
+                        return cellByIndex;
+                    } else {
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @return the lineType
+     */
+    public String getLineType() {
+        return lineType;
+    }
+
+    /**
+     * @param lineType
+     *            the lineType to set
+     */
+    public void setLineType(String lineType) {
+        this.lineType = lineType;
+        if (this.lineTypeControlValue == null)
+            this.lineTypeControlValue = lineType;
+    }
+
+    /**
+     * @return the lineTypeControlValue
+     */
+    public String getLineTypeControlValue() {
+        return lineTypeControlValue;
+    }
+
+    /**
+     * @param lineTypeControlValue
+     *            the lineTypeControlValue to set
+     */
+    public void setLineTypeControlValue(String lineTypeControlValue) {
+        this.lineTypeControlValue = lineTypeControlValue;
+        if (this.lineType == null)
+            this.lineType = lineTypeControlValue;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SchemaLine lineType=");
+        sb.append(this.lineType);
+        if (this.lineTypeControlValue != null) {
+            sb.append(" lineTypeControlValue=");
+            sb.append(this.lineTypeControlValue);
+        }
+        sb.append(" occurs=");
+        if (isOccursInfinitely())
+            sb.append("INFINITE");
+        else
+            sb.append(this.occurs);
+        return sb.toString();
+    }
+
+    /**
+     * Writes a line to the writer. Each cell is identified from the schema by the name of the cell.
+     * If the schema-cell has no name, the cell at the same position in the line is used under the
+     * condition that it also lacks name.
+     * 
+     * If the schema-cell has a name the cell with the same name is used. If no such cell is found
+     * and the cell at the same position lacks name, it is used instead.
+     * 
+     * If no corresponding cell is found for a schema-cell, the cell is left empty. Sub-class
+     * decides how to treat empty cells.
+     * 
+     * @param line
+     * @param writer
+     * @throws IOException
+     * @throws JSaParException
+     */
+    abstract public void output(Line line, Writer writer) throws IOException, JSaParException;
+
+    /**
+     * Reads characters from the line and generates a LineParsedEvent to the listener when it has
+     * been parsed.
+     * 
+     * @param nLineNumber
+     *            The current line number while parsing.
+     * @param sLine
+     *            The line to parse
+     * @param listener
+     *            The listener to generate call-backs to.
+     * @return true if a line was found. false if there were no cells within this line.
+     * @throws IOException
+     * @throws JSaParException
+     */
+    abstract boolean parse(long lineNumber, String line, ParsingEventListener listener) throws JSaParException,
+            IOException;
+
+    /**
+     * @return the ignoreReadEmptyLines
+     */
+    public boolean isIgnoreReadEmptyLines() {
+        return ignoreReadEmptyLines;
+    }
+
+    /**
+     * @param ignoreEmptyLines
+     */
+    public void setIgnoreReadEmptyLines(boolean ignoreEmptyLines) {
+        this.ignoreReadEmptyLines = ignoreEmptyLines;
+    }
+
+    /**
+     * Handles behavior of empty lines
+     * 
+     * @param lineNumber
+     * @param listener
+     * @return Returns true (always).
+     * @throws JSaParException
+     */
+    protected boolean handleEmptyLine(long lineNumber, ParsingEventListener listener) throws JSaParException {
+        if (!isIgnoreReadEmptyLines()) {
+            listener.lineParsedEvent(new LineParsedEvent(this, new Line(getLineType()), lineNumber));
+        }
+        return true;
+    }
+
+}
