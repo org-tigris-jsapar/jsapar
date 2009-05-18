9dcb5053df360c4515f824cbdfa3f303d9dceba4
stenix71 <p.jonas.stenberg@gmail.com>
2009-05-18 10:37:17 +0000

    Build jsapar-1.1.1-2

---
+++ jsapar-1.1.1-2/src/org/jsapar/input/ParseSchema.java
@@ -0,0 +1,22 @@
+package org.jsapar.input;
+
+import java.io.IOException;
+
+import org.jsapar.JSaParException;
+
+
+public interface ParseSchema {
+
+    /**
+     * This method should only be called by a Parser class. Don't use this
+     * directly in your code. Use a Parser instead.
+     * 
+     * @param reader
+     * @param listener
+     * @throws IOException
+     * @throws JSaParException
+     */
+    public abstract void parse(java.io.Reader reader, ParsingEventListener listener)
+	    throws IOException, JSaParException;
+
+}
