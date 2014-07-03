ac8624d7b803acb826ec43cc640a273f45fa200d
stenix71 <p.jonas.stenberg@gmail.com>
2014-07-03 13:46:11 +0000

    Removed obsolete import.

--- src/main/java/org/jsapar/schema/Xml2SchemaBuilder.java
+++ src/main/java/org/jsapar/schema/Xml2SchemaBuilder.java
@@ -2,7 +2,6 @@ package org.jsapar.schema;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
