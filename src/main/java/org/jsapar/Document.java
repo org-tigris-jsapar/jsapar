cbcd3128777aa335a02f3f98ecbedf3f2ba47281
stenix71 <p.jonas.stenberg@gmail.com>
2015-02-07 14:01:40 +0000

    Improved Document interface almost according to suggestions from R

--- src/main/java/org/jsapar/Document.java
+++ src/main/java/org/jsapar/Document.java
@@ -1,7 +1,9 @@
 package org.jsapar;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * A document contains multiple lines where each line corresponds to a line of
@@ -18,13 +20,13 @@ public class Document implements Serializable {
      * 
      */
     private static final long serialVersionUID = 6098681751483565285L;
-    java.util.ArrayList<Line> lines = null;
+    ArrayList<Line> lines = null;
 
     /**
      * Creates an empty document.
      */
     public Document() {
-        this.lines = new java.util.ArrayList<Line>();
+        this.lines = new ArrayList<>();
     }
 
     /**
@@ -34,7 +36,7 @@ public class Document implements Serializable {
      * @param nInitialCapacity
      */
     public Document(int nInitialCapacity) {
-        this.lines = new java.util.ArrayList<Line>(nInitialCapacity);
+        this.lines = new ArrayList<>(nInitialCapacity);
     }
 
     /**
@@ -47,32 +49,37 @@ public class Document implements Serializable {
      * @see #getLineIterator()
      */
     @SuppressWarnings("unchecked")
-    public java.util.Collection<Line> getLines() {
-        return (java.util.Collection<Line>) lines.clone();
+    public List<Line> getLines() {
+        return (List<Line>) lines.clone();
     }
 
     /**
      * Returns the line at the specified index. First line has index 0.
      * @param index
      * @return The line at the specified index. 
+     * @throws IndexOutOfBoundsException
+     *             - if the index is out of range (index < 0 || index >= size())
      */
     public Line getLine(int index) {
         return this.lines.get(index);
     }
 
     /**
-     * Removes the first line and returns it.
+     * Removes the first line of this document and returns it.
      * 
      * @return The line that was just removed.
+     * @throws IndexOutOfBoundsException
+     *             - if the document is empty, i.e. it has not lines.
      */
     public Line removeFirstLine() {
-        return this.lines.remove(0);
+        return removeLineAt(0);
     }
 
     /**
-     * Adds a line last in the list of lines.
+     * Adds the given line at the end of the collection of lines within the document.
      * 
      * @param line
+     *            The line to add.
      */
     public void addLine(Line line) {
         this.lines.add(line);
@@ -107,4 +114,95 @@ public class Document implements Serializable {
         }
         return sb.toString();
     }
+    
+    /**
+     * @return True if there are no lines within this document, false otherwise.
+     */
+    public boolean isEmpty() {
+        return lines.isEmpty();
+    }
+
+    /**
+     * This implementation is quite slow if there are a lot of lines and the line type asked for is not among the top
+     * lines. It will iterate through all lines in order to find supplied line type.
+     * 
+     * @param lineType
+     *            The line type to test.
+     * @return True if there is at least one line of the supplied line type, false otherwise.
+     */
+    public boolean containsLineType(String lineType) {
+        return findFirstLineOfType(lineType) != null;
+    }
+
+    /**
+     * This implementation is quite slow if there are a lot of lines and the line type asked for is not among the top
+     * lines. It will iterate through all lines in order to find supplied line type.
+     * 
+     * @param lineType
+     *            The line type to find first line of.
+     * @return The first line in the document that has the supplied line type.
+     */
+    public Line findFirstLineOfType(String lineType) {
+        assert lineType != null;
+        for (Line line : lines) {
+            if (lineType.equals(line.getLineType()))
+                return line;
+        }
+        return null;
+    }
+    
+    /**
+     * @param lineType
+     * @return A list of all lines with a line type that equals supplied line type or an empty list if no such line
+     *         exist within this document.
+     */
+    public List<Line> findLinesOfType(String lineType) {
+        List<Line> result = new ArrayList<>();
+        assert lineType != null;
+
+        for (Line line : lines) {
+            if (lineType.equals(line.getLineType()))
+                result.add(line);
+        }
+        return result;
+    }
+
+    /**
+     * @return The number of lines within this document. The same as getNumberOfLines()
+     */
+    public int size() {
+        return lines.size();
+    }
+
+    /**
+     * Removes supplied line from this document. Only if the supplied line instance is part of the document it will be
+     * removed since equality will be tested upon same instance and not by using equals() method.
+     * 
+     * @param line
+     * @return true if line was found and removed from document. False otherwise.
+     */
+    public boolean removeLine(Line line) {
+        assert line != null;
+        Iterator<Line> i = lines.iterator();
+        while (i.hasNext()) {
+            if (i.next() == line) {
+                i.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Removes line at supplied index.
+     * 
+     * @param index
+     * @return The line that was removed.
+     * @throws IndexOutOfBoundsException
+     *             - if the index is out of range (index < 0 || index >= size())
+     */
+    public Line removeLineAt(int index){
+        return lines.remove(index);
+    }
+    
 }
