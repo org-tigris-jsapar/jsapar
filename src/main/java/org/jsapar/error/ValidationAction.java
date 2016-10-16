6d3818605182322cebed166dde6566b2e52249d6
stenix71 <p.jonas.stenberg@gmail.com>
2016-10-16 09:30:15 +0000

    Improved javadocs.

--- src/main/java/org/jsapar/error/ValidationAction.java
+++ src/main/java/org/jsapar/error/ValidationAction.java
@@ -1,8 +1,28 @@
 package org.jsapar.error;
 
 /**
- * Created by stejon0 on 2016-07-03.
+ * Enum describing different actions to take upon validation.
+ * @see org.jsapar.parse.ParseConfig
+ * @see org.jsapar.compose.bean.BeanComposerConfig
  */
 public enum ValidationAction {
-    ERROR, EXCEPTION, IGNORE_LINE, NONE
+    /**
+     * Generate an error event.
+     */
+    ERROR,
+
+    /**
+     * Throw an exception
+     */
+    EXCEPTION,
+
+    /**
+     * Silently ignore the current line
+     */
+    IGNORE_LINE,
+
+    /**
+     * Do nothing, which in most cases is the same as silently ignoring the current line.
+     */
+    NONE
 }
