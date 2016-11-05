a51bcba9ccf39477e170a08cce7dd720f378f379
stenix71 <p.jonas.stenberg@gmail.com>
2016-11-05 19:12:55 +0000

    Code cleanup and javadocs.

--- src/main/java/org/jsapar/utils/StringUtils.java
+++ src/main/java/org/jsapar/utils/StringUtils.java
@@ -4,82 +4,84 @@ public class StringUtils {
 
     /**
      * Removes all characters of specified type from the string
-     * 
-     * @param s
-     *            The string to remove from
-     * @param chToRemove
-     *            The character to remove
+     *
+     * @param s          The string to remove from
+     * @param chToRemove The character to remove
      * @return The new string where all character of type chToRemove has been
-     *         removed.
+     * removed.
      */
     public static String removeAll(String s, char chToRemove) {
-	if (s.indexOf(chToRemove) >= 0)
-	    return removeAll(new StringBuilder(s), chToRemove).toString();
-	else
-	    return s;
+        if (s.indexOf(chToRemove) >= 0)
+            return removeAll(new StringBuilder(s), chToRemove).toString();
+        else
+            return s;
     }
 
     /**
      * Removes all characters of specified type from the string builder
-     * 
-     * @param sb
-     *            The string to remove from
-     * @param chToRemove
-     *            The character to remove
+     *
+     * @param sb         The string to remove from
+     * @param chToRemove The character to remove
      * @return The new string builder where all character of type chToRemove has
-     *         been removed.
+     * been removed.
      */
     public static StringBuilder removeAll(StringBuilder sb, char chToRemove) {
-	final String sToRemove = Character.toString(chToRemove);
-	int nIndex = sb.indexOf(sToRemove);
+        final String sToRemove = Character.toString(chToRemove);
+        int nIndex = sb.indexOf(sToRemove);
 
-	while (nIndex >= 0) {
-	    sb.deleteCharAt(nIndex);
-	    nIndex = sb.indexOf(sToRemove, nIndex);
-	}
-	return sb;
+        while (nIndex >= 0) {
+            sb.deleteCharAt(nIndex);
+            nIndex = sb.indexOf(sToRemove, nIndex);
+        }
+        return sb;
     }
 
     /**
      * Removes all characters that are regarded as white-space according to
      * Character.isWhitespace(int codePoint) function.
-     * 
-     * @param s
+     *
+     * @param s The string to remove all white-spaces from
      * @return A string without any white-space characters.
      */
     public static String removeAllWhitespaces(String s) {
-	StringBuilder sb = new StringBuilder(s);
-	for (int i = 0; i < sb.length(); i++) {
-	    int codePoint = sb.codePointAt(i);
-	    if (Character.isWhitespace(codePoint)) {
-		for (int k = 0; k < Character.charCount(codePoint); k++) {
-		    sb.deleteCharAt(i);
-		}
-		i--;
-	    }
-	}
-	return sb.toString();
+        return removeAll(s, Character::isWhitespace);
     }
 
     /**
      * Removes all characters that are regarded as space according to
      * Character.isSpaceChar(int codePoint) function.
-     * 
-     * @param s
+     *
+     * @param s The string to remove all spaces from
      * @return A string without any space characters.
      */
     public static String removeAllSpaces(String s) {
-	StringBuilder sb = new StringBuilder(s);
-	for (int i = 0; i < sb.length(); i++) {
-	    int codePoint = sb.codePointAt(i);
-	    if (Character.isSpaceChar(codePoint)) {
-		for (int k = 0; k < Character.charCount(codePoint); k++) {
-		    sb.deleteCharAt(i);
-		}
-		i--;
-	    }
-	}
-	return sb.toString();
+        return removeAll(s, Character::isSpaceChar);
+    }
+
+    private interface CheckCharacterType{
+        boolean check(int codePoint);
+    }
+
+    /**
+     * Removes all characters that are regarded as space according to
+     * provided check
+     *
+     * @param s        String to remove from.
+     * @param check    Check lambda, if returns true, character will be removed.
+     * @return A string without any space characters.
+     */
+    private static String removeAll(String s, CheckCharacterType check) {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < sb.length(); i++) {
+            int codePoint = sb.codePointAt(i);
+            if (check.check(codePoint)) {
+                for (int k = 0; k < Character.charCount(codePoint); k++) {
+                    sb.deleteCharAt(i);
+                }
+                i--;
+            }
+        }
+        return sb.toString();
     }
 
 }
