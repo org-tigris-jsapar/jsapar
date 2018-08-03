38c462d5281b86c5eb77e6f4b2c021dcb0703453
stenix71 <p.jonas.stenberg@gmail.com>
2018-08-03 14:38:47 +0200

    Made SchemaException inherit from JSaParException, thus making it an unchecked exception.

--- src/main/java/org/jsapar/schema/SchemaException.java
+++ src/main/java/org/jsapar/schema/SchemaException.java
@@ -3,16 +3,14 @@
  */
 package org.jsapar.schema;
 
+import org.jsapar.error.JSaParException;
+
 /**
  * Exception used for when there are errors within a schema or while building a schema.
  */
-public class SchemaException extends Exception {
+public class SchemaException extends JSaParException {
 
     private static final long serialVersionUID = 636516160510599949L;
-
-    public SchemaException() {
-        super();
-    }
 
     public SchemaException(String s) {
         super(s);
