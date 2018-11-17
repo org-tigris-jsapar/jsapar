af413ed3d14dfd36be91ffdcfe02355a6de896d1
Jonas Stenberg <p.jonas.stenberg@gmail.com>
2018-11-17 12:00:49 +0100

    Changed method signature of text to xml converter to a more standardized argument order.

--- src/main/java/org/jsapar/Text2XmlConverter.java
+++ src/main/java/org/jsapar/Text2XmlConverter.java
@@ -36,7 +36,7 @@ public class Text2XmlConverter {
     public void convert(Reader reader, Writer writer) {
         try {
             Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformXml(transformer, writer, reader);
+            transformXml(reader, writer, transformer);
         } catch (TransformerException e) {
             throw new JSaParException("Failed to transform text input to xml", e);
         }
@@ -44,37 +44,37 @@ public class Text2XmlConverter {
 
     /**
      * Converts a text input to an xml output and applies supplied xslt to the output xml.
-     *
-     * @param reader     The reader to read text input from.
-     * @param xsltReader Reader to the xslt.
+     *  @param reader     The reader to read text input from.
      * @param writer     The writer to write xml to.
+     * @param xsltReader Reader to the xslt.
      */
-    public void convert(Reader reader, Reader xsltReader, Writer writer) {
+    public void convert(Reader reader, Writer writer, Reader xsltReader) {
         try {
             Transformer transformer = TransformerFactory.newInstance().newTransformer(new StreamSource(xsltReader));
-            transformXml(transformer, writer, reader);
+            transformXml(reader, writer, transformer);
         } catch (TransformerException e) {
             throw new JSaParException("Failed to transform text input to xml", e);
         }
     }
 
-    private void transformXml(Transformer transformer, Writer writer, Reader reader) throws TransformerException {
+    private void transformXml(Reader reader, Writer writer, Transformer transformer) throws TransformerException {
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         transformer.setOutputProperty(OutputKeys.METHOD, "xml");
         transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
 
-        transform(transformer, writer, reader);
+        transform(reader, writer, transformer);
     }
 
     /**
      * Transforms the text input to any text output using the supplied transformer.
      *
-     * @param transformer The transformer to use during the transformation.
-     * @param writer      The reader to read text input from.
      * @param reader      The writer to write the text output to.
+     * @param writer      The reader to read text input from.
+     * @param transformer The transformer to use during the transformation.
      * @throws TransformerException In case of transformation error.
      */
-    public void transform(Transformer transformer, Writer writer, Reader reader) throws TransformerException {
+    @SuppressWarnings("WeakerAccess")
+    public void transform(Reader reader, Writer writer, Transformer transformer) throws TransformerException {
         transformer.transform(new SAXSource(saxReader, new InputSource(reader)), new StreamResult(writer));
     }
 
