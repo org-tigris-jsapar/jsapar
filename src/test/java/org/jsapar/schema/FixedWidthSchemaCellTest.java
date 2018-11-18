ff61552d41c9e90abe2816831a3f3cb383fd9819
Jonas Stenberg <p.jonas.stenberg@gmail.com>
2018-11-18 17:30:47 +0100

    Added unit tests

--- src/test/java/org/jsapar/schema/FixedWidthSchemaCellTest.java
+++ src/test/java/org/jsapar/schema/FixedWidthSchemaCellTest.java
@@ -4,6 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -20,7 +24,7 @@ public class FixedWidthSchemaCellTest {
 
 
     @Test
-    public final void testClone() throws CloneNotSupportedException {
+    public final void testClone() {
         FixedWidthSchemaCell schemaCell = new FixedWidthSchemaCell("First name", 11);
         schemaCell.setAlignment(FixedWidthSchemaCell.Alignment.RIGHT);
 
@@ -31,6 +35,35 @@ public class FixedWidthSchemaCellTest {
 
         // Does not clone strings values yet. Might do that in the future.
         assertTrue(schemaCell.getName() == clone.getName());
+    }
+
+    @Test
+    public final void testAlignmentFit() throws IOException {
+        assertEquals("12345", applyFit(FixedWidthSchemaCell.Alignment.LEFT, 5, "123456789"));
+        assertEquals("56789", applyFit(FixedWidthSchemaCell.Alignment.RIGHT, 5, "123456789"));
+        assertEquals("34567", applyFit(FixedWidthSchemaCell.Alignment.CENTER, 5, "123456789"));
+
+    }
+
+    private String applyFit(FixedWidthSchemaCell.Alignment alignment, int length, String value) throws IOException {
+        StringWriter writer = new StringWriter();
+        alignment.fit(writer, length, value);
+        return writer.toString();
+    }
+
+    @Test
+    public void testSetGetLength() {
+        FixedWidthSchemaCell schemaCell = new FixedWidthSchemaCell("First name", 11, FixedWidthSchemaCell.Alignment.LEFT, '*');
+        assertEquals('*', schemaCell.getPadCharacter());
+        assertEquals(11, schemaCell.getLength());
+        schemaCell.setLength(5);
+        assertEquals(5, schemaCell.getLength());
+    }
+
+    @Test
+    public void testToString() {
+        FixedWidthSchemaCell schemaCell = new FixedWidthSchemaCell("First name", 11, FixedWidthSchemaCell.Alignment.LEFT, '*');
+        assertEquals("SchemaCell name='First name' cellFormat=CellType=STRING length=11 alignment=LEFT", schemaCell.toString());
     }
 
 }
