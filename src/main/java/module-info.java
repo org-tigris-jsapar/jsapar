defc229c330fb8120c692fd7e7db23bcb424e830
stenix71 <p.jonas.stenberg@gmail.com>
2019-10-18 16:39:50 +0200

    Added java module info.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.jsapar {
+    requires java.desktop;
+    exports org.jsapar;
+    exports org.jsapar.compose;
+    exports org.jsapar.compose.bean;
+    exports org.jsapar.concurrent;
+    exports org.jsapar.convert;
+    exports org.jsapar.error;
+    exports org.jsapar.model;
+    exports org.jsapar.parse;
+    exports org.jsapar.schema;
+}
