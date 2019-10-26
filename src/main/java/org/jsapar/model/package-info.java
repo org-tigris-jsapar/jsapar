c8453cf634e8b0f8fcbaa6a22c760402a00a145d
Jonas Stenberg <p.jonas.stenberg@gmail.com>
2019-10-26 12:30:03 +0200

    Moved release specific config to the release-profile that is run by the release plugin.

--- src/main/java/org/jsapar/model/package-info.java
+++ src/main/java/org/jsapar/model/package-info.java
@@ -8,7 +8,7 @@
  * You can use this model to build a structure that you later compose to an output by using a {@link org.jsapar.compose.Composer}.
  * <p>
  * While using a {@link org.jsapar.parse.ParseTask}, this model will be the result, either as separate
- * {@link org.jsapar.model.Line} objects in {@link javax.sound.sampled.LineEvent} or as a complete {@link org.jsapar.model.Document}.
+ * {@link org.jsapar.model.Line} objects in {@link org.jsapar.parse.LineParsedEvent} or as a complete {@link org.jsapar.model.Document}.
  *
  * In order to make it easier to retrieve and alter cell values within a {@link org.jsapar.model.Line}, you may use the {@link org.jsapar.model.LineUtils} class.
  */
